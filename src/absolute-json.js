454801b59e2f656ed7132a8f6ce3c0cbb873ac4b
Danny Ralde <danny.ralde@etermax.com>
2016-03-16 14:51:33 -0300

    add AMD COMMONJS MODULE SUPPORT

--- src/absolute-json.js
+++ src/absolute-json.js
@@ -1,187 +1,181 @@
-/*
- absolute-json
- version 0.7
- author: Leandro Cabrera
- https://github.com/lean/absolute-json
- Licensed under the MIT license.
- */
-;(function (root,$) {
+(function (root,$) {
+  var abjson = {},
 
-    var abjson = {};
+  // defaults
+  options = {
+    source: null,
+    sourceUrl: "",
+    localeObject: {},
+    customJsonParser: null
+  };
 
-    // defaults
-    var options = {
-        source: null,
-        sourceUrl: "",
-        localeObject: {},
-        customJsonParser : null
+  // helpers
+  function AbjError(name, message) {
+    this.name = name;
+    this.message = message || "error";
+  }
+
+  AbjError.prototype = new Error();
+  AbjError.prototype.constructor = AbjError;
+
+  function forEach(obj, iterator, context) {
+    var i, key;
+    if (obj === null) {
+      return;
     }
-
-    // helpers
-    function AbjError ( name, message ) {
-        this.name = name;
-        this.message = message || "error";
-    }
-
-    AbjError.prototype = new Error();
-    AbjError.prototype.constructor = AbjError;
-
-    function forEach (obj, iterator, context) {
-        if (obj == null) return;
-        if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
-            obj.forEach(iterator, context);
-        } else if (obj.length === +obj.length) {
-            for (var i = 0, l = obj.length; i < l; i++) {
-                if (iterator.call(context, obj[i], i, obj) === {}) return;
-            }
-        } else {
-            for (var key in obj) {
-                if ( obj.hasOwnProperty(key) ) {
-                    if (iterator.call(context, obj[key], key, obj) === {}) return;
-                }
-            }
+    if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+      obj.forEach(iterator, context);
+    } else if (obj.length === +obj.length) {
+      for (i = 0, l = obj.length; i < l; i++) {
+        if (iterator.call(context, obj[i], i, obj) === {}) {
+          return;
         }
-    }
-
-    function getPropertyByString(obj,str) {
-        var a = str.split('.');
-        while (a.length) {
-            var n = a.shift();
-            if (n in obj) {
-                obj = obj[n];
-            } else {
-                return;
-            }
-        }
-        return obj;
-    }
-
-    function load ( opt, callback ) {
-
-        options.source = opt.source;
-        options.sourceUrl = opt.sourceUrl;
-
-        loadSource( callback );
-    }
-
-    function loadSource ( callback ) {
-
-        if (options.source){
-            options.localeObject = options.source;
-            callback();
+      }
+    } else {
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (iterator.call(context, obj[key], key, obj) === {}) {
             return;
+          }
         }
+      }
+    }
+  }
 
-        $.ajax({
-            url: options.sourceUrl,
-            type: 'get',
-            dataType: 'json',
-            cache : false,
-            success: function(data){
-                options.localeObject = data;
-                callback();
-            },
-            error: function ( xhr, textStatus ) {
-                callback(new AbjError(textStatus.toUpperCase(), xhr.statusText));
-            }
-        });
+  function getPropertyByString(obj,str) {
+    var n, a = str.split('.');
+    while (a.length) {
+      n = a.shift();
+      if (n in obj) {
+        obj = obj[n];
+      } else {
+        return;
+      }
+    }
+    return obj;
+  }
+
+  function load(opt, callback) {
+    options.source = opt.source;
+    options.sourceUrl = opt.sourceUrl;
+
+    loadSource(callback);
+  }
+
+  function loadSource(callback) {
+    if (options.source) {
+      options.localeObject = options.source;
+      callback();
+      return;
     }
 
-    function get ( key ) {
-        var r;
+    $.ajax({
+      url: options.sourceUrl,
+      type: 'get',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        options.localeObject = data;
+        callback();
+      },
+      error: function (xhr, textStatus) {
+        callback(new AbjError(textStatus.toUpperCase(), xhr.statusText));
+      }
+    });
+  }
 
-        if(options.localeObject[key]){
+  function get(key) {
+    var r;
 
-            r =  wildcardReplace(options.localeObject[key], Array.prototype.slice.call(arguments, 1));
-
-        }else{
-
-            if(key.indexOf(".")>0){
-                r = wildcardReplace( getPropertyByString(options.localeObject,key), Array.prototype.slice.call(arguments, 1) );
-            }
-
-        }
-
-        return r;
-
+    if (options.localeObject[key]) {
+      r = wildcardReplace(options.localeObject[key], Array.prototype.slice.call(arguments, 1));
+    } else {
+      if (key.indexOf(".") > 0) {
+        r = wildcardReplace(getPropertyByString(options.localeObject, key), Array.prototype.slice.call(arguments, 1));
+      }
     }
 
-    function updateElements ( el, opt ) {
+    return r;
+  }
 
-        var elKey = el.attr( "data-abjson" ),
-            updateElementsdText = get( elKey );
+  function updateElements(el, opt) {
+    var elKey = el.attr("data-abjson"),
+        updateElementsdText = get(elKey);
 
-        if ( typeof updateElementsdText !== "undefined" ) {
-
-            if ( updateElementsdText === Object(updateElementsdText) ) {
-
-                forEach( updateElementsdText, function ( val, key ) {
-
-                    if ( key === "text" ){
-
-                        if ( el.attr( "data-abjson-r" ) ) {
-                            el.html( wildcardReplace( updateElementsdText, el.attr( "data-abjson-r" ).split("|") ) );
-                        } else {
-                            el.html( val );
-                        }
-
-                    } else {
-
-                        el.attr( key,val );
-
-                    }
-
-                });
-
+    if (typeof updateElementsdText !== "undefined") {
+      if (updateElementsdText === Object(updateElementsdText)) {
+        forEach(updateElementsdText, function (val, key) {
+          if (key === "text") {
+            if (el.attr("data-abjson-r")) {
+              el.html(wildcardReplace(updateElementsdText, el.attr("data-abjson-r").split("|")));
             } else {
-
-                if ( el.attr( "data-abjson-r" ) ) {
-                    el.html( wildcardReplace( updateElementsdText, el.attr( "data-abjson-r" ).split("|") ) );
-                } else {
-                    el.html( updateElementsdText );
-                }
-
+              el.html(val);
             }
-
+          } else {
+            el.attr(key, val);
+          }
+        });
+      } else {
+        if (el.attr("data-abjson-r")) {
+          el.html(wildcardReplace(updateElementsdText, el.attr("data-abjson-r").split("|")));
         } else {
-            el.html(elKey + " NOT FOUND");
+          el.html(updateElementsdText);
         }
+      }
+    } else {
+      el.html(elKey + " NOT FOUND");
+    }
+  }
 
+  function wildcardReplace(text, replaceElements) {
+    var i,
+        replacedText = text;
+
+    for (i = 0; i < replaceElements.length; i++) {
+      replacedText = replacedText.replace(new RegExp("%" + (i + 1), 'ig'), replaceElements[i]);
     }
 
-    function wildcardReplace ( text, replaceElements ) {
+    return replacedText;
+  }
 
-        var i,
-            replacedText = text;
+  if (!root.abjson) {
+    $.fn.abjson = function (options) {
+      var elements = $("[data-abjson]", this);
 
-        for( i=0; i < replaceElements.length; i++ ) {
-            replacedText = replacedText.replace( new RegExp("%" + (i+1), 'ig'), replaceElements[i] )
-        }
+      elements.each(function () {
+        updateElements($(this), options);
+      });
+    };
 
-        return replacedText;
+    root.abjson = root.abjson || abjson;
+  }
 
-    }
+  // public interface
+  abjson.load = load;
+  abjson.options = options;
+  abjson.get = get;
 
-    if (!root.abjson) {
+  /**
+   * Add support for AMD (Asynchronous Module Definition) libraries such as require.js.
+   */
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return {
+        abjson: abjson
+      };
+    });
+  }
 
-        $.fn.abjson = function ( options ) {
+  /**
+   * Add support for CommonJS libraries such as browserify.
+   */
+  if (typeof exports !== 'undefined') {
+    exports.abjson = abjson;
+  }
 
-            var elements = $("[data-abjson]", this);
+  // define globally in case AMD is not available or available but not used
 
-            elements.each( function () {
-                updateElements( $(this), options );
-            });
-
-        };
-
-        root.abjson = root.abjson || abjson;
-    }
-
-
-    // public interface
-    abjson.load = load;
-    abjson.options = options;
-    abjson.get = get;
-
-
+  if (typeof window !== 'undefined') {
+    window.abjson = abjson;
+  }
 })(window, jQuery);
